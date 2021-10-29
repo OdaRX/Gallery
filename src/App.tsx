12cb3b776face44e48155465bf03c5845ca0d071
unknown <chromegold70@gmail.com>
2021-10-29 20:12:37 -0300

    ADD photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,85 @@
+import { FormEvent, useEffect, useState } from 'react'
 import * as C from './App.styles'
+import GlobalStyles from './styles/GlobalStyles'
+import * as Photos from './services/photos'
+import { Photo } from './@Types/Photo'
+import { PhotoItem } from './components/PhotoItem'
 
 const App = () => {
-  return <C.Container>...</C.Container>
+  const [uploading, setUploading] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState<Photo[]>([])
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true)
+      setPhotos(await Photos.getAll())
+      setLoading(false)
+    }
+    getPhotos()
+  }, [])
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+
+    const file = formData.get('image') as File
+
+    if (file && file.size > 0) {
+      setUploading(true)
+      let result = await Photos.insert(file)
+      setUploading(false)
+
+      if (result instanceof Error) {
+        alert(`${result.name} - ${result.message}`)
+      } else {
+        let newPhotoList = [...photos]
+        newPhotoList.push(result)
+        setPhotos(newPhotoList)
+      }
+    }
+  }
+
+  return (
+    <>
+      <C.Container>
+        <C.Area>
+          <C.Header>Galeria de fotos</C.Header>
+
+          <C.UploadForm onSubmit={handleFormSubmit}>
+            <input type="file" name="image" />
+            <input type="submit" value="Enviar" />
+            {uploading && 'Enviando...'}
+          </C.UploadForm>
+
+          {loading && (
+            <C.ScreenWarning>
+              <div className="emoji">⏳</div>
+              <div>Carregando...</div>
+            </C.ScreenWarning>
+          )}
+
+          {!loading && photos.length >= 0 && (
+            <C.PhotoList>
+              {photos.map((item, index) => (
+                <PhotoItem key={index} url={item.url} name={item.name} />
+              ))}
+            </C.PhotoList>
+          )}
+
+          {!loading && photos.length === 0 && (
+            <C.ScreenWarning>
+              <div className="emoji">😢</div>
+              <div>Não há fotos cadastradas.</div>
+            </C.ScreenWarning>
+          )}
+        </C.Area>
+      </C.Container>
+
+      <GlobalStyles />
+    </>
+  )
 }
 
 export default App
